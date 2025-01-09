36a002a0b8f92eb210b00d9637be10b21d2b8520
Lydia-Lu-code <Lydia-Lu@LydiadeMacBook-Air.local>
2025-01-09 17:09:23 +0800

    Initial Commit

---
+++ MovieGem/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieGem
+//
+//  Created by Lydia Lu on 2025/1/9.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
